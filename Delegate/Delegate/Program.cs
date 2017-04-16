e5e4a589054c0c6448bde8df3aa291c2219c23f4
Garfield Lee <garfield_550@outlook.com>
2017-04-16 15:22:11 +0800

    Delegate: Delegate test.

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegate
+{
+    delegate void ConsoleWriteLine(string value);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleWriteLine cwl;
+            cwl = new ConsoleWriteLine(MyConsole.WriteLine);
+
+            cwl("Hello world!");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyConsole
+    {
+        public static void WriteLine(string value)
+        {
+            Console.WriteLine("****************");
+            Console.WriteLine(value);
+            Console.WriteLine("****************");
+        }
+    }
+}
